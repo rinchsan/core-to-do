addb4acd8a15d800c675f0daa7234061cdebcbed
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-01-31 01:18:01 +0900

    追加カテゴリを保存するUserDefaultsをアプリ初回起動時に作成

--- CoreToDo for Qiita/CoreToDo for Qiita/AppDelegate.swift
+++ CoreToDo for Qiita/CoreToDo for Qiita/AppDelegate.swift
@@ -17,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // set added categories array for User Defaults
+        let defaults = UserDefaults.standard
+        defaults.set([], forKey: "addedCategories")
+        print(defaults.array(forKey: "addedCategories")!)
+        
         return true
     }
 
