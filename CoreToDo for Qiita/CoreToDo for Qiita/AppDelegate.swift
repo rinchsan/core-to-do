4e4c175f9e0622d19563ff53a706507b5763e346
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-03-23 04:07:17 +0900

    Set notification center delegate

--- CoreToDo for Qiita/CoreToDo for Qiita/AppDelegate.swift
+++ CoreToDo for Qiita/CoreToDo for Qiita/AppDelegate.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
+
+        UNUserNotificationCenter.current().delegate = self
+
         return true
     }
 
